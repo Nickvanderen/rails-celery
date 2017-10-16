0801a78ba3e5ade59c1b1f479bced327c4552981
Eliza Brine & Nick Van Deren <pair+ebrine.nickvanderen@devbootcamp.com>
2017-10-16 16:55:44 -0700

    Creates questions model migration, controller, routes to index

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171016182213) do
+ActiveRecord::Schema.define(version: 20171016235231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string   "topic"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
