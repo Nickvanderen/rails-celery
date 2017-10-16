42fb60e79160ebaf05439c2ea0b34c83f01a3410
Eliza Brine & Nick Van Deren <pair+ebrine.nickvanderen@devbootcamp.com>
2017-10-16 16:10:45 -0700

    Creates session controller, session routes, login page. Implements session#create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,13 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_username(params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
   end
 
   def destroy
