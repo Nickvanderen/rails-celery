0db6bf7bb8169fd8dab7437c66c1973288da33dd
Eliza Brine & Nick Van Deren <pair+ebrine.nickvanderen@devbootcamp.com>
2017-10-16 17:02:25 -0700

    Creates questions index page with login/register/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
   resources :questions, only: [:index]
 
-  root 'questions#index'
 
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
