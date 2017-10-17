98113c6aea4652c4e98b8116bedff33200375817
Eliza Brine & Nick Van Deren <pair+ebrine.nickvanderen@devbootcamp.com>
2017-10-16 17:59:23 -0700

    Adds show route for questions. Links on question index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:new, :create]
-  resources :questions, only: [:index]
+  resources :questions, only: [:index, :show]
 
 
   get    '/login',   to: 'sessions#new'
